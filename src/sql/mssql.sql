bc46617947dc199117f6ab90e50c4e5da998dad9
Manuel Laflamme <mlaflamm@@users.sourceforge.net>
2003-04-13 02:07:38 +0000

    Modified EMPTY_MULTITYPE_TABLE to use DATETIME as data type for column TIMESTAMP_COL. This fix binary issue with TIMESTAMP data type.

--- src/sql/mssql.sql
+++ src/sql/mssql.sql
@@ -1,115 +1,74 @@
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[EMPTY_MULTITYPE_TABLE]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
-drop table [dbo].[EMPTY_MULTITYPE_TABLE]
-GO
+-----------------------------------------------------------------------------
+-- TEST_TABLE
+-----------------------------------------------------------------------------
 
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[EMPTY_TABLE]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
-drop table [dbo].[EMPTY_TABLE]
-GO
+CREATE TABLE TEST_TABLE
+  (COLUMN0 VARCHAR(32),
+   COLUMN1 VARCHAR(32),
+   COLUMN2 VARCHAR(32),
+   COLUMN3 VARCHAR(32));
 
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[IDENTITY_TABLE]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
-drop table [dbo].[IDENTITY_TABLE]
-GO
+-----------------------------------------------------------------------------
+-- SECOND_TABLE
+-----------------------------------------------------------------------------
 
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[ONLY_PK_TABLE]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
-drop table [dbo].[ONLY_PK_TABLE]
-GO
+CREATE TABLE SECOND_TABLE
+  (COLUMN0 VARCHAR(32),
+   COLUMN1 VARCHAR(32),
+   COLUMN2 VARCHAR(32),
+   COLUMN3 VARCHAR(32));
 
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[PK_TABLE]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
-drop table [dbo].[PK_TABLE]
-GO
+-----------------------------------------------------------------------------
+-- EMPTY_TABLE
+-----------------------------------------------------------------------------
 
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[SECOND_TABLE]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
-drop table [dbo].[SECOND_TABLE]
-GO
+CREATE TABLE EMPTY_TABLE
+  (COLUMN0 VARCHAR(32),
+   COLUMN1 VARCHAR(32),
+   COLUMN2 VARCHAR(32),
+   COLUMN3 VARCHAR(32));
 
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[TEST_TABLE]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
-drop table [dbo].[TEST_TABLE]
-GO
+-----------------------------------------------------------------------------
+-- PK_TABLE
+-----------------------------------------------------------------------------
 
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[TEST_IDENTITY_NOT_PK]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
-drop table [dbo].[TEST_IDENTITY_NOT_PK]
-GO
+CREATE TABLE PK_TABLE
+  (PK0 NUMERIC(38, 0) NOT NULL,
+   PK1 NUMERIC(38, 0) NOT NULL,
+   PK2 NUMERIC(38, 0) NOT NULL,
+   NORMAL0 VARCHAR(32),
+   NORMAL1 VARCHAR(32), PRIMARY KEY (PK0, PK1, PK2));
 
-CREATE TABLE [dbo].[EMPTY_MULTITYPE_TABLE] (
-    [VARCHAR_COL] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL ,
-    [NUMERIC_COL] [numeric](38, 0) NULL ,
-    [TIMESTAMP_COL] [timestamp] NULL ,
-    [VARBINARY_COL] [varbinary] (254) NULL 
-) ON [PRIMARY]
-GO
+-----------------------------------------------------------------------------
+-- ONLY_PK_TABLE
+-----------------------------------------------------------------------------
 
-CREATE TABLE [dbo].[EMPTY_TABLE] (
-    [COLUMN0] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL ,
-    [COLUMN1] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL ,
-    [COLUMN2] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL ,
-    [COLUMN3] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL 
-) ON [PRIMARY]
-GO
+CREATE TABLE ONLY_PK_TABLE
+  (PK0 NUMERIC(38, 0) NOT NULL PRIMARY KEY);
 
-CREATE TABLE [dbo].[IDENTITY_TABLE] (
-    [IDENTITY_TABLE_ID] [int] IDENTITY (1, 1) NOT NULL ,
-    [COLUMN0] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL ,
-    [COLUMN1] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL 
-) ON [PRIMARY]
-GO
+-----------------------------------------------------------------------------
+-- EMPTY_MULTITYPE_TABLE
+-----------------------------------------------------------------------------
 
-ALTER TABLE dbo.IDENTITY_TABLE ADD CONSTRAINT
-  PK_IDENTITY_TABLE PRIMARY KEY CLUSTERED 
-  (
-  IDENTITY_TABLE_ID
-  ) ON [PRIMARY]
+CREATE TABLE EMPTY_MULTITYPE_TABLE
+  (VARCHAR_COL VARCHAR(32),
+   NUMERIC_COL NUMERIC(38, 0),
+   TIMESTAMP_COL DATETIME,
+   VARBINARY_COL VARBINARY(254));
 
-GO
+-----------------------------------------------------------------------------
+-- IDENTITY_TABLE
+-----------------------------------------------------------------------------
 
-CREATE TABLE [dbo].[ONLY_PK_TABLE] (
-    [PK0] [numeric](38, 0) NOT NULL 
-) ON [PRIMARY]
-GO
+CREATE TABLE IDENTITY_TABLE
+  (IDENTITY_TABLE_ID INT IDENTITY (1, 1) NOT NULL,
+   COLUMN0 VARCHAR(32),
+   COLUMN1 VARCHAR(32), PRIMARY KEY (IDENTITY_TABLE_ID));
 
-ALTER TABLE dbo.ONLY_PK_TABLE ADD CONSTRAINT
-  PK_ONLY_PK_TABLE PRIMARY KEY CLUSTERED 
-  (
-  PK0
-  ) ON [PRIMARY]
+-----------------------------------------------------------------------------
+-- IDENTITY_TABLE
+-----------------------------------------------------------------------------
 
-GO
-
-CREATE TABLE [dbo].[PK_TABLE] (
-    [PK0] [numeric](38, 0) NOT NULL ,
-    [PK1] [numeric](38, 0) NOT NULL ,
-    [PK2] [numeric](38, 0) NOT NULL ,
-    [NORMAL0] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL ,
-    [NORMAL1] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL 
-) ON [PRIMARY]
-GO
-ALTER TABLE dbo.PK_TABLE ADD CONSTRAINT
-  PK_PK_TABLE PRIMARY KEY CLUSTERED 
-  (
-  PK0,
-  PK1,
-  PK2
-  ) ON [PRIMARY]
-
-GO
-
-CREATE TABLE [dbo].[SECOND_TABLE] (
-    [COLUMN0] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL ,
-    [COLUMN1] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL ,
-    [COLUMN2] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL ,
-    [COLUMN3] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL 
-) ON [PRIMARY]
-GO
-
-CREATE TABLE [dbo].[TEST_TABLE] (
-    [COLUMN0] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL ,
-    [COLUMN1] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL ,
-    [COLUMN2] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL ,
-    [COLUMN3] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL 
-) ON [PRIMARY]
-GO
-CREATE TABLE dbo.TEST_IDENTITY_NOT_PK
-  (
-  COL01 int NOT NULL IDENTITY (1, 1),
-  COL02 varchar(64) NULL
-  )  ON [PRIMARY]
-GO
+CREATE TABLE TEST_IDENTITY_NOT_PK
+  (COL01 INT IDENTITY (1, 1) NOT NULL,
+   COL02 VARCHAR(64));
