0a2eb7aff4e34de280fbe7ebb087f0b09898c51d
Manuel Laflamme <mlaflamm@@users.sourceforge.net>
2003-05-17 15:49:07 +0000

    Oups! There is an error at the construction of the DataType.UNKNOWN constant.

--- src/java/org/dbunit/dataset/datatype/DataType.java
+++ src/java/org/dbunit/dataset/datatype/DataType.java
@@ -33,8 +33,9 @@ import java.sql.PreparedStatement;
  */
 public abstract class DataType
 {
-    public static final DataType UNKNOWN = new UnknownDataType(
-            "UNKNOWN", Types.OTHER);
+    public static final DataType UNKNOWN = new UnknownDataType();
+//    public static final DataType UNKNOWN = new UnknownDataType(
+//            "UNKNOWN", Types.OTHER);
 
     public static final DataType CHAR = new StringDataType(
             "CHAR", Types.CHAR);
