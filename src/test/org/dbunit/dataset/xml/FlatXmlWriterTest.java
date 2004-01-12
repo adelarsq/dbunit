46ab4e6eedd695678b4e4eab148e190ca53408ce
Manuel Laflamme <mlaflamm@@users.sourceforge.net>
2004-01-12 18:32:46 +0000

    Fixed class header

--- src/test/org/dbunit/dataset/xml/FlatXmlWriterTest.java
+++ src/test/org/dbunit/dataset/xml/FlatXmlWriterTest.java
@@ -1,3 +1,24 @@
+/*
+ *
+ * DbUnit Database Testing Framework
+ * Copyright (C)2002, Manuel Laflamme
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 package org.dbunit.dataset.xml;
 
 import org.dbunit.dataset.DefaultTableMetaData;
@@ -16,8 +37,8 @@ import org.xml.sax.InputSource;
 
 /**
  *
- * <p> Copyright (c) 2002 OZ.COM.  All Rights Reserved. </p>
- * @author manuel.laflamme$
+ * @author Manuel Laflamme
+ * @version $Revision$
  * @since Sep 8, 2003$
  */
 public class FlatXmlWriterTest extends TestCase
