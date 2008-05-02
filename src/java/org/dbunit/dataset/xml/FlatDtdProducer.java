e4b0ea1cfd65c019bf6aba972fbe6c68ed6d5fe9
Roberto Lo Giacco <rlogiacco@users.sourceforge.net>
2008-05-02 17:08:31 +0000

    [ 1953115 ] End of table name cut off when bracketed in FlatDtdProducer Submitted by: Mike Norrish

--- src/java/org/dbunit/dataset/xml/FlatDtdProducer.java
+++ src/java/org/dbunit/dataset/xml/FlatDtdProducer.java
@@ -251,24 +251,7 @@ public class FlatDtdProducer implements IDataSetProducer, EntityResolver, DeclHa
                 while (tokenizer.hasMoreTokens()) {
                     String tableName = tokenizer.nextToken();
 
-                    // Prune ending occurrence operator
-                    if (tableName.endsWith("*") || tableName.endsWith("?") || tableName.endsWith("+")) {
-                        tableName = tableName.substring(0, tableName.length() - 1);
-                    }
-
-					// Interestingly, sometimes a table comes in brackets, e.g. "(mytablename)"
-					// These must be removed because otherwise the list of columns associated with the table
-					// can not be retrieved from the map, i.e. the list of columns (columnList) is null.
-					// This leads to a NullPointerException in DB-Unit 2.2 when columnList is being accessed.
-					// Patch for NullPointerException starts here
-                    if (tableName.startsWith("(")) {
-                        tableName = tableName.substring(1, tableName.length() - 1);
-                    }
-
-                    if (tableName.endsWith(")")) {
-                        tableName = tableName.substring(0, tableName.length() - 1);
-                    }
-                    // Patch for NullPointerException ends here
+                    tableName = cleanupTableName(tableName);
 
                     List columnList = (List)_columnListMap.get(tableName);
                     Column[] columns = (Column[])columnList.toArray(new Column[0]);
@@ -286,6 +269,21 @@ public class FlatDtdProducer implements IDataSetProducer, EntityResolver, DeclHa
 
             throw new SAXException(e);
         }
+    }
+    
+    protected String cleanupTableName(String tableName)
+    {
+        String cleaned = tableName;
+        while (cleaned.startsWith("(")) {
+            cleaned = cleaned.substring(1);
+        }
+        while (cleaned.endsWith(")")
+                || cleaned.endsWith("*")
+                || cleaned.endsWith("?")
+                || cleaned.endsWith("+")) {
+            cleaned = cleaned.substring(0, cleaned.length() - 1);
+        }
+        return cleaned;
     }
 
     public void startEntity(String name) throws SAXException
