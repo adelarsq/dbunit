affe79999b2a4cf30545c831ff844492ed29f328
Matthias Gommeringer <gommma@users.sourceforge.net>
2008-08-29 08:38:12 +0000

    Simplification of CollectionsHelper - use JDK methods "toArray" instead of manually iteration over the Set.

--- src/java/org/dbunit/util/CollectionsHelper.java
+++ src/java/org/dbunit/util/CollectionsHelper.java
@@ -21,88 +21,81 @@
 
 package org.dbunit.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Iterator;
 import java.util.Set;
 
 import org.apache.commons.collections.set.ListOrderedSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Helper for collections-related methods.
  * <br>
  * @author Felipe Leme (dbunit@felipeal.net)
- * @version $Revision$
+ * @author gommma (gommma AT users.sourceforge.net)
+ * @author Last changed by: $Author$
+ * @version $Revision$ $Date$
  * @since Nov 5, 2005
- * 
  */
-
 public class CollectionsHelper {
 
     /**
      * Logger for this class
      */
     private static final Logger logger = LoggerFactory.getLogger(CollectionsHelper.class);
-  
-  // class is "static"
-  private CollectionsHelper() {}
 
-  /**
-   * Returns a Set from an array of objects.
-   * Note the Iterator returned by this Set preserves the order of the array.
-   * @param objects array of objects
-   * @return Set with the elements of the array or null if entry is null
-   */
-  public static Set objectsToSet( Object[] objects ) {
+    // class is "static"
+    private CollectionsHelper() {}
+
+    /**
+     * Returns a Set from an array of objects.
+     * Note the Iterator returned by this Set preserves the order of the array.
+     * @param objects array of objects
+     * @return Set with the elements of the array or null if entry is null
+     */
+    public static Set objectsToSet( Object[] objects ) {
         logger.debug("objectsToSet(objects={}) - start", objects);
 
-    if ( objects == null ) {
-      return null;
+        if ( objects == null ) {
+            return null;
+        }
+        Set set = new ListOrderedSet();
+        for (int i = 0; i < objects.length; i++) {
+            set.add(objects[i]);
+        }
+        return set;
     }
-    Set set = new ListOrderedSet();
-    for (int i = 0; i < objects.length; i++) {
-      set.add(objects[i]);
-    }
-    return set;
-  }
 
-  /**
-   * Returns an array of Objects from a Set.
-   * @param set a Set 
-   * @return array of Objects with the elements of the Set or null if set is null
-   */
-  public static Object[] setToObjects( Set set ) {
+    /**
+     * Returns an array of Objects from a Set.
+     * @param set a Set 
+     * @return array of Objects with the elements of the Set or null if set is null
+     */
+    public static Object[] setToObjects( Set set ) {
         logger.debug("setToObjects(set={}) - start", set);
 
-    if ( set == null ) {
-      return null;
+        if ( set == null ) {
+            return null;
+        }
+        else {
+            return set.toArray();
+        }
     }
-    Object[] objects = new Object[ set.size() ];
-    int i=0;
-    for (Iterator iter = set.iterator(); iter.hasNext(); i++) {
-      objects[i] = iter.next();      
-    }
-    return objects;
-  }
 
-  /**
-   * Returns an array of Strings from a Set.
-   * @param set a Set of Strings
-   * @return array of Strings with the elements of the Set or null if set is null
-   */
-  public static String[] setToStrings( Set set ) {
+    /**
+     * Returns an array of Strings from a Set.
+     * @param set a Set of Strings
+     * @return array of Strings with the elements of the Set or null if set is null
+     */
+    public static String[] setToStrings( Set set ) {
         logger.debug("setToStrings(set={}) - start", set);
 
-    if ( set == null ) {
-      return null;
-    }
-    String[] strings = new String[ set.size() ];
-    int i=0;
-    for (Iterator iter = set.iterator(); iter.hasNext(); i++) {
-      strings[i] = (String) iter.next();      
-    }
-    return strings;
-  };
+        if ( set == null ) {
+            return null;
+        }
+        else {
+            String[] strings = (String[]) set.toArray(new String[0]);
+            return strings;
+        }
+    };
 
 }
