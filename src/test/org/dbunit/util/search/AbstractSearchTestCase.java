834a0a62c114261930180d294a1f84d82413b3be
Matthias Gommeringer <gommma@users.sourceforge.net>
2008-08-29 08:48:18 +0000

    Cosmetics - fixed code indentation

--- src/test/org/dbunit/util/search/AbstractSearchTestCase.java
+++ src/test/org/dbunit/util/search/AbstractSearchTestCase.java
@@ -29,104 +29,103 @@ import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-import org.apache.commons.collections.set.ListOrderedSet;
-
 import junit.framework.TestCase;
+
+import org.apache.commons.collections.set.ListOrderedSet;
 
 /**
  * @author Felipe Leme (dbunit@felipeal.net)
- * @version $Revision$
+ * @author Last changed by: $Author$
+ * @version $Revision$ $Date$
  * @since Aug 25, 2005
  */
-
 public abstract class AbstractSearchTestCase extends TestCase {
 
-  protected static final String A = "A";
-  protected static final String B = "B";
-  protected static final String C = "C";
-  protected static final String D = "D";
-  protected static final String E = "E";
-  protected static final String F = "F";
+    protected static final String A = "A";
+    protected static final String B = "B";
+    protected static final String C = "C";
+    protected static final String D = "D";
+    protected static final String E = "E";
+    protected static final String F = "F";
 
-  // fixtures
-  protected final Map fEdgesPerNodeMap = new HashMap();
-  
-  protected final Set fAllEdgesSet = new HashSet();
+    // fixtures
+    protected final Map fEdgesPerNodeMap = new HashMap();
 
-  protected final Set fExpectedOutput = new ListOrderedSet();
+    protected final Set fAllEdgesSet = new HashSet();
 
-  protected final Set fInput = new HashSet();
+    protected final Set fExpectedOutput = new ListOrderedSet();
 
-  protected final DepthFirstSearch fSearch = new DepthFirstSearch();
+    protected final Set fInput = new HashSet();
 
-  protected void doIt() throws Exception {
-    Set actualOutput = fSearch.search(this.fInput, getCallback());
-    assertEquals("sizes does not match", this.fExpectedOutput.size(),
-        actualOutput.size());
-    Iterator iterExpected = this.fExpectedOutput.iterator();
-    Iterator iterActual = actualOutput.iterator();
-    int i = 0;
-    while (iterExpected.hasNext()) {
-      Object expectedNode = iterExpected.next();
-      Object actualNode = iterActual.next();
-      assertEquals("mismatched element at position " + i, expectedNode,
-          actualNode);
-      i++;
+    protected final DepthFirstSearch fSearch = new DepthFirstSearch();
+
+    protected void doIt() throws Exception {
+        Set actualOutput = fSearch.search(this.fInput, getCallback());
+        assertEquals("sizes does not match", this.fExpectedOutput.size(),
+                actualOutput.size());
+        Iterator iterExpected = this.fExpectedOutput.iterator();
+        Iterator iterActual = actualOutput.iterator();
+        int i = 0;
+        while (iterExpected.hasNext()) {
+            Object expectedNode = iterExpected.next();
+            Object actualNode = iterActual.next();
+            assertEquals("mismatched element at position " + i, expectedNode,
+                    actualNode);
+            i++;
+        }
     }
-  }
 
-  protected void setInput(String[] nodes) {
-    for (int i = 0; i < nodes.length; i++) {
-      this.fInput.add(nodes[i]);
+    protected void setInput(String[] nodes) {
+        for (int i = 0; i < nodes.length; i++) {
+            this.fInput.add(nodes[i]);
+        }
     }
-  }
 
-  protected void setOutput(String[] nodes) {
-    for (int i = 0; i < nodes.length; i++) {
-      this.fExpectedOutput.add(nodes[i]);
+    protected void setOutput(String[] nodes) {
+        for (int i = 0; i < nodes.length; i++) {
+            this.fExpectedOutput.add(nodes[i]);
+        }
     }
-  }
 
-  protected void addEdges(String from, String[] tos) {
-    Set tmpEdges = new TreeSet();
-    for (int i = 0; i < tos.length; i++) {
-      Edge edge = new Edge(from, tos[i]);
-      this.fAllEdgesSet.add( edge );
-      tmpEdges.add(edge);
+    protected void addEdges(String from, String[] tos) {
+        Set tmpEdges = new TreeSet();
+        for (int i = 0; i < tos.length; i++) {
+            Edge edge = new Edge(from, tos[i]);
+            this.fAllEdgesSet.add( edge );
+            tmpEdges.add(edge);
+        }
+        this.fEdgesPerNodeMap.put(from, tmpEdges);
     }
-    this.fEdgesPerNodeMap.put(from, tmpEdges);
-  }
 
-  protected ISearchCallback getCallback() {
-    return new ISearchCallback() {
-      public SortedSet getEdges(Object fromNode) {
-        return getEdgesFromNode(fromNode);
-      };
+    protected ISearchCallback getCallback() {
+        return new ISearchCallback() {
+            public SortedSet getEdges(Object fromNode) {
+                return getEdgesFromNode(fromNode);
+            };
 
-      public void nodeAdded(Object fromNode) {
-      }
+            public void nodeAdded(Object fromNode) {
+            }
 
-      public boolean searchNode(Object node) {
-        return true;
-      }
+            public boolean searchNode(Object node) {
+                return true;
+            }
+        };
+    }
+
+    protected SortedSet getEdgesFromNode(Object fromNode) {
+        return (SortedSet) fEdgesPerNodeMap.get(fromNode);
+    }
+
+    protected SortedSet getEdgesToNode(Object toNode) {
+        TreeSet set = new TreeSet();
+        Iterator iterator = this.fAllEdgesSet.iterator();
+        while ( iterator.hasNext() ) {
+            Edge edge = (Edge) iterator.next();
+            if ( edge.getTo().equals(toNode)) {
+                set.add( edge );
+            }
+        }
+        return set;
     };
-  }
-
-  protected SortedSet getEdgesFromNode(Object fromNode) {
-    ;
-    return (SortedSet) fEdgesPerNodeMap.get(fromNode);
-  }
-
-  protected SortedSet getEdgesToNode(Object toNode) {
-    TreeSet set = new TreeSet();
-    Iterator iterator = this.fAllEdgesSet.iterator();
-    while ( iterator.hasNext() ) {
-      Edge edge = (Edge) iterator.next();
-      if ( edge.getTo().equals(toNode)) {
-        set.add( edge );
-      }
-    }
-    return set;
-  };
 
 }
