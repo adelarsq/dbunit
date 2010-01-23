7f68200f2b4154e45421ea3363a88afd9969c00e
John Hurst <jbhurst@users.sourceforge.net>
2010-01-23 22:23:25 +0000

    [2938029] - Corrected filename of xmlTableTest.xml.

--- dbunit/src/test/org/dbunit/util/fileloader/DataFileLoaderFullXmlTest.java
+++ dbunit/src/test/org/dbunit/util/fileloader/DataFileLoaderFullXmlTest.java
@@ -46,7 +46,7 @@ public class DataFileLoaderFullXmlTest extends TestCase {
      * Test can load the specified file.
      */
     public void testLoad() throws DataSetException {
-        String filename = "/XmlTableTest.xml";
+        String filename = "/xmlTableTest.xml";
         IDataSet ds = loader.load(filename);
         assertTrue("No tables found in dataset.", ds.getTableNames().length > 0);
         // DataSet loading tests verify data accuracy
